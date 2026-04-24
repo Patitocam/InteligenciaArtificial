4cc76e02ca1ae4120d8e190640b6fa9799f312e4
laureanoan0 <laureanomeyer22@gmail.com>
2026-04-24 14:04:57 -0300

    Flee behavior

--- Assets/Scenes/Enemy/EnemySM.cs
+++ Assets/Scenes/Enemy/EnemySM.cs
@@ -21,7 +21,7 @@ public class EnemySM
         var idle = new IdleStateEnemy(this, fsm);
         fsm.AddState(idle, EnemyStatesEnum.Idle);
         fsm.AddState(new PatrolStateEnemy(this, fsm), EnemyStatesEnum.Patrolling);
-        fsm.AddState(new RunAwayStateEnemy(this, fsm, Target), EnemyStatesEnum.RunAway);
+        fsm.AddState(new RunAwayStateEnemy(this, fsm, Target, owner, speed), EnemyStatesEnum.RunAway);
         fsm.AddState(new ChaseStateEnemy(this, fsm, Target, rb, owner, speed), EnemyStatesEnum.Chasing);
         fsm.AddState(new AttackStateEnemy(this, fsm, Target), EnemyStatesEnum.Attack);
         fsm.AddState(new ArriveStateEnemy(this, fsm, Target, owner, arriveDistance, attackRange, speed), EnemyStatesEnum.Arrive);
--- Assets/Scenes/Enemy/RunAwayEnemyState.cs
+++ Assets/Scenes/Enemy/RunAwayEnemyState.cs
@@ -1,23 +1,43 @@
 ﻿using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+using static UnityEngine.UI.GridLayoutGroup;
 
 namespace Assets.Scenes.Enemy
 {
     public class RunAwayStateEnemy : EnemyStates
     {
         private EnemySM sm;
+        GameObject target;
+        EnemyController owner;
+        float speed;
 
-        public RunAwayStateEnemy(EnemySM sm, GenericStateMachine<EnemyStatesEnum> stateMachine, GameObject Target) : base(stateMachine)
+        public RunAwayStateEnemy(EnemySM sm, GenericStateMachine<EnemyStatesEnum> stateMachine, GameObject Target, EnemyController owner, float speed) : base(stateMachine)
         {
             this.sm = sm;
+            this.target = Target;
+            this.owner = owner;
+            this.speed = speed; 
         }
         public override void Tick(float deltaTime)
         {
             base.Tick(deltaTime);
-            RunAway();
+            Move();
         }
-        private void RunAway()
+
+        private void Move()
         {
-            Debug.Log("Im Running away");
+            owner.Move(RunAway(), speed);
+            owner.transform.LookAt(target.transform.position);
+        }
+
+        private Vector3 RunAway()
+        {
+            Vector3 toTarget = target.transform.position - owner.transform.position;
+            float distance = toTarget.magnitude;
+
+            Vector3 desiredDir = ((toTarget - owner.transform.position).normalized) * -1;
+
+            return desiredDir;
         }
     }
 }
